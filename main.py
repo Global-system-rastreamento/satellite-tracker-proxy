7301741e21483a9acfe4083218be4b6fb5380f4a
juan <juan.bisposantos05@gmail.com>
2025-08-22 19:41:08 -0300

    Tratamento de exceções

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from xml.etree.ElementTree import ElementTree
+from xml.etree import ElementTree
 import datetime
 
 app = Flask(__name__)
@@ -8,4 +8,14 @@ app = Flask(__name__)
 def receive_messages():
     xml_data = request.data
 
-    
+    try:
+        pass
+
+    except ElementTree.ParseError as e:
+        print(f"Erro ao analisar o XML: {e}")
+        response_xml = """<?xml version="1.0" encoding="UTF-8"?><stuResponseMsg><state>fail</state><stateMessage>Invalid XML format.</stateMessage></stuResponseMsg>"""
+        return response_xml, 400, {'Content-Type': 'text/xml'}
+    except Exception as e:
+        print(f"Ocorreu um erro: {e}")
+        response_xml = """<?xml version="1.0" encoding="UTF-8"?><stuResponseMsg><state>fail</state><stateMessage>An internal error occurred.</stateMessage></stuResponseMsg>"""
+        return response_xml, 500, {'Content-Type': 'text/xml'}
