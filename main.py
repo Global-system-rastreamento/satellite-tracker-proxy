42584c0509dea63dc23aa6a6b7d4e3ead13ae6d2
juan <juan.bisposantos05@gmail.com>
2025-08-22 19:51:28 -0300

    Organização

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ def receive_messages():
         print(f"Erro ao analisar o XML: {e}")
         response_xml = """<?xml version="1.0" encoding="UTF-8"?><stuResponseMsg><state>fail</state><stateMessage>Invalid XML format.</stateMessage></stuResponseMsg>"""
         return response_xml, 400, {'Content-Type': 'text/xml'}
+    
     except Exception as e:
         print(f"Ocorreu um erro: {e}")
         response_xml = """<?xml version="1.0" encoding="UTF-8"?><stuResponseMsg><state>fail</state><stateMessage>An internal error occurred.</stateMessage></stuResponseMsg>"""
