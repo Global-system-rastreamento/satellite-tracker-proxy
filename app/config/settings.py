89d426e3c79aa89485e19b7fc9e32c2550607dc5
juan <juan.bisposantos05@gmail.com>
2025-08-23 11:32:07 -0300

    add configs do servidor final

---
+++ app/config/settings.py
@@ -0,0 +1,14 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore",
+    )
+
+    LOG_LEVEL: str = "INFO"
+
+    MAIN_SERVER_ADDRESS: tuple = ()
+
+settings = Settings()
