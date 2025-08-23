ee52dd68f204fa634b715b68bd91ff26d3493333
juan <juan.bisposantos05@gmail.com>
2025-08-23 11:31:38 -0300

    criando um módulo para conexão com nosso server final, um server TCP que traduz protocolos, vamos mokar o comportamento de um rastreador enviando pacotes

---
+++ app/connection/main_server_conection.py
@@ -0,0 +1,24 @@
+import socket
+import json
+
+from app.config.settings import settings
+
+def send_dict_to_server(data):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect(settings.MAIN_SERVER_ADDRESS)
+
+        message = json.dumps(data)
+        message_encoded = message.encode('utf-8')
+
+        message_ecapsulated = b"\xff" + message_encoded + b"\xfe"
+
+        sock.sendall(message_ecapsulated)
+        print(f"Message Sent!\nSTRING: {message}\nENCODED: {message_ecapsulated}")
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+    finally:
+        if sock:
+            sock.close()
