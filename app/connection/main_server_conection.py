b85f404257cb1f73fee20d98041f76f104757f54
juan <juan.bisposantos05@gmail.com>
2025-08-25 14:02:26 -0300

    metodos principais

--- app/connection/main_server_conection.py
+++ app/connection/main_server_conection.py
@@ -15,17 +15,49 @@ class MainServerConnection:
         self.heartbeat_interval = 5
         self.heartbeat_timer = None
 
-        message = json.dumps(data)
-        message_encoded = message.encode('utf-8')
+    def connect(self):
+        try:
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.socket.connect((self.host, self.port))
+            print(f"Connected to main server at {self.host}:{self.port}")
+            self.running = True
+            self.start_heartbeat_timer()
+        except Exception as e:
+            print(f"Error connecting to main server: {e}")
 
-        message_ecapsulated = b"\xff" + message_encoded + b"\xfe"
+    def disconnect(self):
+        self.running = False
+        if self.socket:
+            self.socket.close()
+            print("Disconnected from main server")
+        if self.heartbeat_timer:
+            self.heartbeat_timer.cancel()
 
-        sock.sendall(message_ecapsulated)
-        print(f"Message Sent!\nSTRING: {message}\nENCODED: {message_ecapsulated}")
+    def send_data(self, data):
+        try:
+            while not self.running:
+                self.connect()
 
-    except Exception as e:
-        print(f"Error: {e}")
+            message = json.dumps(data)
+            message_encoded = message.encode('utf-8')
+            message_ecapsulated = b"\xff" + message_encoded + b"\xfe"
+            self.socket.sendall(message_ecapsulated)
+            print(f"Message Sent!\nSTRING: {message}\nENCODED: {message_ecapsulated}")
+            self.reset_heartbeat_timer()
+        except Exception as e:
+            print(f"Error sending data: {e}")
 
-    finally:
-        if sock:
-            sock.close()
+    def start_heartbeat_timer(self):
+        self.heartbeat_timer = threading.Timer(self.heartbeat_interval, self.send_heartbeat)
+        self.heartbeat_timer.daemon = True
+        self.heartbeat_timer.start()
+
+    def reset_heartbeat_timer(self):
+        if self.heartbeat_timer:
+            self.heartbeat_timer.cancel()
+        self.start_heartbeat_timer()
+
+    def send_heartbeat(self):
+        if self.running:
+            heartbeat_data = {"message_type": "heartbeat"}
+            self.send_data(heartbeat_data)
